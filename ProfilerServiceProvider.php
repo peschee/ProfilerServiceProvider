8be59e8642bd992d7ea22962ecc351591c89846b
Peter Siska <pesche@gridonic.ch>
2011-10-15 21:41:47 +0200

    Fixed the way the twig extension and the corresponding view is added. Other extensions should not be overloaded. Also, checking if twig extension is set, otherwise doing nothig.

--- ProfilerServiceProvider.php
+++ ProfilerServiceProvider.php
@@ -17,39 +17,53 @@ class ProfilerServiceProvider implements ServiceProviderInterface {
         if (!isset($app['profiler.cookie_name'])) {
             $app['profiler.cookie_name'] = 'webprofiler';
         }
-        $app->get($app['profiler.data_url'], function() use($app){
-            $data = unserialize(urldecode($app['request']->cookies->get($app['profiler.cookie_name'])));
-            return $app['twig']->render('__profiler-data.twig', array(
-                'data' => $data
-            ));
-        });
-        $loader = new \Twig_Loader_Filesystem(__DIR__.'/Resources/views/');
-        $app['twig.loader']->addLoader($loader);
 
-        $app['dispatcher']->addListener('silex.after', function($event) use($app) {
-            $data = array();
-            $request = $event->getRequest();
-            $response = $event->getResponse();
-            $data['request'] = array(
-                'method'            => $request->getMethod(),
-                'uri'               => $request->getRequestUri(),
-                'format'            => $request->getRequestFormat(),
-                'accept'            => $request->headers->get('accept'),
-                'accept-charset'    => $request->headers->get('accept-charset'),
-                'accept-encoding'   => $request->headers->get('accept-encoding'),
-                'accept-language'   => $request->headers->get('accept-language'),
-                'connection'        => $request->headers->get('connection'),
-                'host'              => $request->headers->get('host'),
-                'user-agent'        => $request->headers->get('user-agent'),
-            );
-            $data['response'] = array(
-                'code'          => $response->getStatusCode(),
-                'cache-control' => $response->headers->get('cache-control')
-            );
-            $event->getResponse()->headers->setCookie(new Cookie($app['profiler.cookie_name'], urlencode(serialize($data))));
-        });
+        if (isset($app['twig'])) {
+            $app->get($app['profiler.data_url'], function() use($app){
+                $data = unserialize(urldecode($app['request']->cookies->get($app['profiler.cookie_name'])));
+                return $app['twig']->render('__profiler-data.twig', array(
+                    'data' => $data
+                ));
+            });
 
-        $app['twig']->addExtension(new ProfilerExtension($app['twig']));
+            $app['dispatcher']->addListener('silex.after', function($event) use($app) {
+                $data = array();
+                $request = $event->getRequest();
+                $response = $event->getResponse();
+                $data['request'] = array(
+                    'method'            => $request->getMethod(),
+                    'uri'               => $request->getRequestUri(),
+                    'format'            => $request->getRequestFormat(),
+                    'accept'            => $request->headers->get('accept'),
+                    'accept-charset'    => $request->headers->get('accept-charset'),
+                    'accept-encoding'   => $request->headers->get('accept-encoding'),
+                    'accept-language'   => $request->headers->get('accept-language'),
+                    'connection'        => $request->headers->get('connection'),
+                    'host'              => $request->headers->get('host'),
+                    'user-agent'        => $request->headers->get('user-agent'),
+                );
+                $data['response'] = array(
+                    'code'          => $response->getStatusCode(),
+                    'cache-control' => $response->headers->get('cache-control')
+                );
+                $event->getResponse()->headers->setCookie(new Cookie($app['profiler.cookie_name'], urlencode(serialize($data))));
+            });
 
+            $oldTwig = $app->raw('twig');
+            $app['twig'] = $app->share(function($c) use ($oldTwig, $app) {
+                $twig = $oldTwig($c);
+                $twig->addExtension(new ProfilerExtension($twig));
+
+                return $twig;
+            });
+
+            $oldLoader = $app->raw('twig.loader.filesystem');
+            $app['twig.loader.filesystem'] = $app->share(function($c) use ($oldLoader, $app) {
+                $loader = $oldLoader($c);
+                $loader->addPath(__DIR__.'/Resources/views');
+
+                return $loader;
+            });
+        }
     }
 }
